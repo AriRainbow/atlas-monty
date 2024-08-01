77089de46a753fc0086b9aabf786396fa8d1c1c6
AriRainbow <mariel.williams@atlasschool.com>
2024-08-01 23:30:33 +0000

    Create monty.h

---
+++ monty.h
@@ -0,0 +1,40 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct_stack_s - Doubly linked list representation of a stack (or queue)
+ * @n: Integer
+ * @prev: Points to the previous element of the stack (or queue)
+ * @next: Points to the next element of the stack (or queue)
+ *
+ * Description: Doubly linked list node structure for stacks,
+ * queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - Opcode and its function
+ * @opcode: The opcode
+ * @f: Function to handle the opcode
+ *
+ * Description: Opcode and its function for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void push(stack_t **stack, unsigned int line_number, char *args);
+void pall(stack_t **stack, unsigned int line_number);
+
+#endif
