617beea94499243a4e6a7ea89e1e6bf3c6864fa6
AriRainbow <mariel.williams@atlasschool.com>
2024-08-05 04:05:20 +0000

    Moving 'add' to correct file

--- opcodes.c
+++ opcodes.c
@@ -142,4 +142,27 @@ void swap(stack_t **stack, unsigned int line_number)
 	*stack = second;
 }
 
+void add(stack_t **stack, unsigned int line_number)
+{
+        stack_t *first;
+        stack_t *second;
+        int sum;
 
+        if (*stack == NULL || (*stack)->next == NULL)
+        {
+                fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+
+        first = *stack;
+        second = (*stack)->next;
+
+        sum = first->n + second->n;
+
+        /* remove top element */
+        *stack = second;
+        (*stack)-> = sum;
+        (*stack)->prev = NULL;
+
+        free(first);
+}
